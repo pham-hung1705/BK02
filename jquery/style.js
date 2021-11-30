78c9a2d632a33b920386e35d3bdc4eee16a90feb
pham-hung1705 <khaipikin0@gmail.com>
2021-11-30 10:51:39 +0700

    Signed-off-by: pham-hung1705 <khaipikin0@gmail.com>

---
+++ jquery/style.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('div').hide();
+    $('button').click(function() {
+        $('div').show();
+    });
+});
+$(function() {
+    $('.btnShow').click(function() {
+        $('div').hide();
+    });
+});
